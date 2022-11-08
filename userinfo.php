e5be9616963b7ae84747e4480cdea45e1737348d
Evilyn Ramos Silva <rms.n.evil@gmail.com>
2022-11-08 19:20:22 -0300

    userinfo

---
+++ userinfo.php
@@ -0,0 +1,9 @@
+<?php
+
+$usersinfo = [
+    ['name'=>'Amanda Teresa', 'cpf'=>'16948769853', 'email'=>'amandatess@hotmail.com', 'password'=>'mandinha'],
+    ['name'=>'Alexis Alexandre', 'cpf'=>'45938211859', 'email'=>'alexis00@hotmail.com', 'password'=>'lex'],
+    ['name'=>'Miguel Guedes', 'cpf'=>'47027234807', 'email'=>'miguel.guedes@yahoo.com', 'password'=>'anjomiguel'],
+    ['name'=>'Charlie Cordeiro', 'cpf'=>'37266262801', 'email'=>'charliecr@outlook.tv', 'password'=>'char1212'],
+    ['name'=>'Nathan Scott', 'cpf'=>'69140990818', 'email'=>'NateScott@gmail.com.br', 'password'=>'nathanbask'],
+];
